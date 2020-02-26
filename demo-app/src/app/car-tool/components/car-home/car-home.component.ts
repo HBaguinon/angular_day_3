04179764a1569fb5be69783d0861bf21f05ed7c9
Harold Baguinon <HBaguinon@gmail.com>
2020-02-26 10:58:15 -0500

    removed multiple subscribes and cleaned up car home

--- demo-app/src/app/car-tool/components/car-home/car-home.component.ts
+++ demo-app/src/app/car-tool/components/car-home/car-home.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
 import { Car } from '../../models/car';
 import { CarsService } from '../../services/cars.service';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-car-home',
@@ -16,15 +20,28 @@ export class CarHomeComponent implements OnInit {
 
   sortColName = '';
 
+  updateCars = {
+    next: cars => {
+      this.cars = cars;
+      this.editCarId = -1;
+    },
+    error: err => {
+      console.log(err);
+    },
+  };
+
   constructor(public carsSvc: CarsService) { }
 
   ngOnInit(): void {
     // this.carsSvc.all().then(cars => {
     //   this.cars = cars;
     // });
-    this.carsSvc.all().subscribe(cars => {
-      this.cars = cars;
-    });
+
+    // this.carsSvc.all().subscribe(cars => {
+    //   this.cars = cars;
+    // });
+
+    this.refreshCars();
   }
 
   doSortCars(sortColName: string) {
@@ -32,27 +49,68 @@ export class CarHomeComponent implements OnInit {
     this.refreshCars();
   }
 
-  refreshCars() {
+  oldRefreshCars() {
     // return this.carsSvc.all(this.sortColName).then(cars => {
     //   this.cars = cars;
     // });
-    return this.carsSvc.all(this.sortColName).subscribe(cars => {
-      this.cars = cars;
-    });
+
+    // return this.carsSvc.all(this.sortColName).subscribe(cars => {
+    //   this.cars = cars;
+    // });
+
+    return this.carsSvc.all(this.sortColName);
+  }
+
+  refreshCars(o: Observable<any> = null) {
+    (!o ? of([]) : o).pipe(switchMap(() => this.carsSvc.all(this.sortColName)))
+      .subscribe(this.updateCars);
   }
 
   doAppendCar(car: Car) {
-    this.carsSvc
-      .append(car)
-      .subscribe(() => this.refreshCars());
-    this.editCarId = -1;
+    // this.carsSvc
+    //   .append(car)
+    //   .subscribe(() => this.refreshCars());
+    // this.editCarId = -1;
+
+    // this.carsSvc
+    //   .append(car)
+    //   .pipe(
+    //     switchMap(() => this.refreshCars())
+    //   )
+    //   .subscribe({
+    //     next: cars => {
+    //       this.cars = cars;
+    //       this.editCarId = -1;
+    //     },
+    //     error: err => {
+    //       console.log(err);
+    //     },
+    //   });
+    this.refreshCars(this.carsSvc.append(car));
   }
 
   doRemoveCar(carId: number) {
-    this.carsSvc
-      .remove(carId)
-      .subscribe(() => this.refreshCars());
-    this.editCarId = -1;
+    // this.carsSvc
+    //   .remove(carId)
+    //   .subscribe(() => this.refreshCars());
+    // this.editCarId = -1;
+
+    // this.carsSvc
+    //   .remove(carId)
+    //   .pipe(
+    //     switchMap(() => this.refreshCars())
+    //   )
+    //   .subscribe({
+    //     next: cars => {
+    //       this.cars = cars;
+    //       this.editCarId = -1;
+    //     },
+    //     error: err => {
+    //       console.log(err);
+    //     },
+    //   });
+
+      this.refreshCars(this.carsSvc.remove(carId));
   }
 
   doEditCar(carId: number) {
@@ -60,10 +118,27 @@ export class CarHomeComponent implements OnInit {
   }
 
   doReplaceCar(car: Car) {
-    this.carsSvc
-      .replace(car)
-      .subscribe(() => this.refreshCars());
-    this.editCarId = -1;
+    // this.carsSvc
+    //   .replace(car)
+    //   .subscribe(() => this.refreshCars());
+    // this.editCarId = -1;
+
+    // this.carsSvc
+    //   .replace(car)
+    //   .pipe(
+    //     switchMap(() => this.refreshCars())
+    //   )
+    //   .subscribe({
+    //     next: cars => {
+    //       this.cars = cars;
+    //       this.editCarId = -1;
+    //     },
+    //     error: err => {
+    //       console.log(err);
+    //     },
+    //   });
+
+    this.refreshCars(this.carsSvc.replace(car));
   }
 
   doCancelCar() {
